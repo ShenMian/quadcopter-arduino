f5ce2002296b18166bdd9a9714a10e75fb63ded5
ShenMian <sms_school@outlook.com>
2023-04-07 09:51:27 +0800

    fix: fix update_motors

--- motors.cpp
+++ motors.cpp
@@ -64,18 +64,20 @@ void update_motors(const EulerAngles& angles, float throttle)
   // 1. writeMicroseconds 理论上可以让电机反转, 但实际上不行, 桨叶的气动结构基本都是设计为单向转的. 反向转的效果只用减小电机力, 让重力实现就行
   // 2. 为了适应不同的电机需要加一些东西, writeMicroseconds 是针对无刷电调的 PWM 控制的, 如果是有刷电调(或普通 MOS 管)可能需要用 analogWrite, 因为 writeMicroseconds 貌似不能做到输出满占空比
 
+  // TODO: 电压跌落补偿
+
   // 假设 1000-1499 为逆时针旋转, 1501-2000 为顺时针旋转, 1500 为停止
 #if QUAD_TYPE == QUAD_X
-  motors[FR].writeMicroseconds(constrain(1500 - (throttle * 400 - angles.pitch + angles.roll - angles.yaw), 1000, 1500));
-  motors[FL].writeMicroseconds(constrain(1500 - (throttle * 400 - angles.pitch - angles.roll + angles.yaw), 1000, 1500));
+  motors[FR].writeMicroseconds(constrain(1500 - (throttle * 400 + angles.pitch + angles.roll + angles.yaw), 1000, 1500));
+  motors[FL].writeMicroseconds(constrain(1500 + (throttle * 400 + angles.pitch - angles.roll - angles.yaw), 1000, 1500));
   
-  motors[BR].writeMicroseconds(constrain(1500 - (throttle * 400 + angles.pitch + angles.roll + angles.yaw), 1000, 1500));
-  motors[BL].writeMicroseconds(constrain(1500 - (throttle * 400 + angles.pitch - angles.roll - angles.yaw), 1000, 1500));
+  motors[BR].writeMicroseconds(constrain(1500 + (throttle * 400 - angles.pitch + angles.roll - angles.yaw), 1000, 1500));
+  motors[BL].writeMicroseconds(constrain(1500 - (throttle * 400 - angles.pitch - angles.roll + angles.yaw), 1000, 1500));
 #else
-  motors[F].writeMicroseconds(constrain(1500 + (throttle * 400 - angles.pitch + angles.yaw), 1500, 2000));
-  motors[B].writeMicroseconds(constrain(1500 + (throttle * 400 + angles.pitch + angles.yaw), 1500, 2000));
+  motors[F].writeMicroseconds(constrain(1500 + (throttle * 400 + angles.pitch - angles.yaw), 1500, 2000));
+  motors[B].writeMicroseconds(constrain(1500 + (throttle * 400 - angles.pitch - angles.yaw), 1500, 2000));
   
-  motors[R].writeMicroseconds(constrain(1500 - (throttle * 400 + angles.roll - angles.yaw), 1000, 1500));
-  motors[L].writeMicroseconds(constrain(1500 - (throttle * 400 - angles.roll - angles.yaw), 1000, 1500));
+  motors[R].writeMicroseconds(constrain(1500 - (throttle * 400 + angles.roll + angles.yaw), 1000, 1500));
+  motors[L].writeMicroseconds(constrain(1500 - (throttle * 400 - angles.roll + angles.yaw), 1000, 1500));
 #endif
 }
