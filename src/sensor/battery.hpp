0d38dccf4dee8474eecd958f841f46f43088fd91
ShenMian <sms_school@outlook.com>
2023-04-25 18:05:25 +0800

    refactor: update battery.hpp

--- src/sensor/battery.hpp
+++ src/sensor/battery.hpp
@@ -7,7 +7,7 @@ class Battery
 {
 public:
 	Battery(uint8_t pin, float voltage_min, float voltage_max, uint8_t resis_1, uint8_t resis_2)
-	    : pin_(pin), voltage_min_(voltage_min), voltage_max_(voltage_max), resis_1_(resis_1), resis_2_(resis_2)
+	    : pin_(pin), volt_min_(voltage_min), volt_max_(voltage_max), resis_1_(resis_1), resis_2_(resis_2)
 	{
 	}
 
@@ -32,14 +32,14 @@ public:
 	 */
 	float get_level() const noexcept
 	{
-		const auto level = map(get_voltage(), voltage_min_, voltage_max_, 0.f, 100.f);
+		const auto level = map(get_voltage(), volt_min_, volt_max_, 0.f, 100.f);
 		return level;
 	}
 
 private:
 	uint8_t pin_;
-	float   voltage_min_;
-	float   voltage_max_;
+	float   volt_min_;
+	float   volt_max_;
 	uint8_t resis_1_; ///< 电阻 1, 单位: kΩ
 	uint8_t resis_2_; ///< 电阻 1, 单位: kΩ
 };
