c9da5ba96a5c125b98c209047614fd8c5f3f0c22
ShenMian <sms_school@outlook.com>
2023-04-25 11:19:02 +0800

    feat: update dc_motor.hpp

--- src/driver/dc_motor.hpp
+++ src/driver/dc_motor.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../actuator/motor.hpp"
+#include "../utility.hpp"
 #include <Arduino.h>
 #include <stdint.h>
 
@@ -12,25 +13,43 @@ class DCMotor : public Motor
 public:
 	DCMotor() = default;
 
-	DCMotor(uint8_t pin)
-	{
-		attach(pin);
-	}
+	DCMotor(uint8_t pin) { attach(pin); }
 
-	void set_speed(float speed) override
-	{
-		if(speed < 0.f || speed > 1.f)
-			abort();
-
-		analogWrite(pin_, map(speed, 0.f, 1.f, 0, 255));
-	}
+	void set_speed(float speed) override { set_duty_cycle(speed); }
 
 	void attach(uint8_t pin) override
 	{
-    pin_ = pin;
+		pin_ = pin;
 		pinMode(pin_, OUTPUT);
 	}
 
+	/**
+	 * @brief 设置 PWM 占空比
+	 *
+	 * @param duty_cycle 占空比, 范围 [0, 1]
+	 */
+	void set_duty_cycle(float duty_cycle)
+	{
+		duty_cycle = clamp(duty_cycle, 0.f, 1.f);
+		analogWrite(pin_, map(duty_cycle, 0.f, 1.f, 0, 255));
+	}
+
+	/**
+	 * @brief 设置角速率
+	 *
+	 * @param rate    角速率, 单位: RPM
+	 * @param voltage 电压
+	 *
+	 * @warning 未测试
+	 */
+	void set_angular_rate(uint8_t rate, float voltage)
+	{
+		const float duty_cycle = rate * internal_resis_ * rated_speed_ / (voltage * 60);
+		set_duty_cycle(duty_cycle);
+	}
+
 private:
-  uint8_t pin_;
+	uint8_t  pin_;
+	uint16_t rated_speed_;
+	float    internal_resis_;
 };
