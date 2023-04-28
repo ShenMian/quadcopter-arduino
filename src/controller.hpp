f8ca560bea8a843237df71c1997e5ab6605f9986
ShenMian <sms_school@outlook.com>
2023-04-28 09:05:30 +0800

    refactor: update controller.hpp

--- src/controller.hpp
+++ src/controller.hpp
@@ -22,7 +22,7 @@ public:
 	 * @param evaluator 评估器
 	 * @param dt        时间变化量
 	 */
-	void update(Estimator& estimator, float dt)
+	void update(const Estimator& estimator, float dt)
 	{
 		const auto angles = estimator.get_angles();
 
@@ -54,7 +54,7 @@ public:
 	 */
 	void set_target_angles(const EulerAngles& angles) noexcept
 	{
-		target_angles_       = angles;
+		target_angles_.yaw   = angles.yaw;
 		target_angles_.pitch = clamp(angles.pitch, -max_pitch_angle_, max_pitch_angle_);
 		target_angles_.roll  = clamp(angles.roll, -max_roll_angle_, max_roll_angle_);
 	}
@@ -62,9 +62,9 @@ public:
 	/**
 	 * @brief 设置目标节流阀
 	 *
-	 * @param target_throttle 目标节流阀, 范围 [0, 1]
+	 * @param throttle 目标节流阀, 范围 [0, 1]
 	 */
-	void set_throttle(float throttle) { target_throttle_ = clamp(throttle, 0.f, 1.f); }
+	void set_throttle(float throttle) noexcept { target_throttle_ = clamp(throttle, 0.f, 1.f); }
 
 private:
 	float       target_throttle_;
